42f5e797d6a5a46ea2e528b0410536ff14c5afae
devymutiara <namikazepian@gmail.com>
2023-10-08 21:13:33 +0700

    empty data

--- resources/views/nasabah/index.blade.php
+++ resources/views/nasabah/index.blade.php
@@ -14,7 +14,7 @@
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">DataTables</li>
+            <li class="breadcrumb-item active">Tabel Data Nasabah</li>
           </ol>
         </div>
       </div>
@@ -29,7 +29,7 @@
         <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title">DataTable with minimal features & hover style</h3>
+              <h3 class="card-title">Berikut table data nasabah</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
@@ -46,48 +46,52 @@
                 </tr>
                 </thead>
                 <tbody>
+                  @if (count($data) === 0)
+                     <p>Data kosong!</p> 
+                  @else
                   @foreach ($data as $d)
-                    <tr>
-                      <td>{{$loop->iteration}}</td>
-                      <td>{{$d->nama}}</td>
-                      <td>{{$d->nama_belakang}}</td>
-                      <td>{{$d->email}}</td>
-                      <td>{{$d->alamat}}</td>
-                      <td>{{$d->telepon}}</td>
-                      <td>
-                        <a href="{{route('client.edit', ['id' => $d->id])}}" class="btn btn-primary"><i class="fas fa-pen"> Edit</i></a>
-                        <a  data-toggle="modal" data-target="#modal-hapus" class="btn btn-danger"><i class="fas fa-trash"> Delete</i></a>
-                      </td>
-                    </tr>
-                    <!-- MODAL -->
-                    <div class="modal fade" id="modal-hapus">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h4 class="modal-title">Default Modal</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                          </div>
-                          <div class="modal-body">
-                            <p>Apakah kamu yakin ingin menghapus data <b>{{$d->name}}</b></p>
-                          </div>
-                          <div class="modal-footer justify-content-between">
-                            <form action="{{route('client.delete', ['id' => $d->id])}}" method="POST">
-                              @csrf
-                              @method('DELETE')
-                              <button type="button" class="btn btn-default" data-dismiss="modal">Tidak</button>
-                              <button type="submit" class="btn btn-primary">Ya, hapus</button>
-                            </form>
-                          </div>
+                  <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$d->nama}}</td>
+                    <td>{{$d->nama_belakang}}</td>
+                    <td>{{$d->email}}</td>
+                    <td>{{$d->alamat}}</td>
+                    <td>{{$d->telepon}}</td>
+                    <td>
+                      <a href="{{route('client.edit', ['id' => $d->id])}}" class="btn btn-primary"><i class="fas fa-pen"> Edit</i></a>
+                      <a  data-toggle="modal" data-target="#modal-hapus" class="btn btn-danger"><i class="fas fa-trash"> Delete</i></a>
+                    </td>
+                  </tr>
+                  <!-- MODAL -->
+                  <div class="modal fade" id="modal-hapus">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h4 class="modal-title">Default Modal</h4>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
                         </div>
-                        <!-- /.modal-content -->
+                        <div class="modal-body">
+                          <p>Apakah kamu yakin ingin menghapus data <b>{{$d->name}}</b></p>
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                          <form action="{{route('client.delete', ['id' => $d->id])}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Tidak</button>
+                            <button type="submit" class="btn btn-primary">Ya, hapus</button>
+                          </form>
+                        </div>
                       </div>
-                      <!-- /.modal-dialog -->
-                    <!-- MODAL -->
-
+                      <!-- /.modal-content -->
                     </div>
-                  @endforeach
+                    <!-- /.modal-dialog -->
+                  <!-- MODAL -->
+
+                  </div>
+                @endforeach
+                @endif
                 </tbody>
               </table>
             </div>
